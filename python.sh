17f2790133d5bf9937494c217e5c1fe124373da6
enfantreble <6405543+enfantreble@users.noreply.github.com>
2023-08-03 13:33:32 +0900

    Update python.sh

--- python.sh
+++ python.sh
@@ -10,3 +10,11 @@ sudo apt update
 sudo apt install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 Compile Python
 sudo make altinstall
+cd /usr/bin
+Remove the current link. This does not delete your older Python 3 version, it only unlinks it from that command.
+sudo rm /usr/bin/python3
+Link the version you want to use from now on
+sudo ln -s /usr/local/bin/python3.10 python3
+Check if everything is as expected. It should now display the version you just installed (3.10.2 for me).
+
+pi@raspberrypi:~ $ python -V
